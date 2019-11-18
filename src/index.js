9b811a2bf844a5ea2ed6ece47997217d23d99d30
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-11-18 11:18:00 -0800

    Setup Project. Yarn add dependencies. Setup file structure. Import dependecies. Create Login component. Create Logic to set token to localStorage.

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { applyMiddleware, createStore } from 'redux';
+import { reducer } from './reducers/index';
+
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import './index.css';
+
+
+import App from './App';
+
+import * as serviceWorker from './serviceWorker';
+
+const store = createStore(reducer,
+    composeWithDevTools(applyMiddleware(thunk))
+)
+
+ReactDOM.render(
+    <Provider store={store} >
+        <Router>
+            <App />
+        </Router>
+    </Provider>
+    , document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
