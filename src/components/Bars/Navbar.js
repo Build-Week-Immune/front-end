5299dd2a179ba551bc65ffae3994b29b67f1f0e1
JonathanChen <jonny29009@yahoo.com>
2019-11-18 15:04:44 -0800

    added file structure

---
+++ src/components/Bars/Navbar.js
@@ -0,0 +1,1 @@
+// Contains the nav bar for all pages
---
+++ src/components/Home/PatientHome.js
@@ -0,0 +1,7 @@
+// Home page for patient
+// Nav bar of this page includes a text 'Immune' at top left, and sandwich icon at top right, which includes a log out button
+// When logging out, user is redirected to home page
+
+// Contains a patient card, which contains summary for the patient; can be edited
+// Contains a see family button, which brings up a family list of family members; can select and turn into patient card; can edit
+// Under the patient card will be a immunization record search query and that patient's immunization list
