fbd42c18d316d719c829836b079a9a8bc3dd3061
JonathanChen <jonny29009@yahoo.com>
2019-11-19 10:18:01 -0800

    deleted signin.js, uncommented lines

--- src/reducers/LoginReducer.js
+++ src/reducers/LoginReducer.js
@@ -1,34 +1,34 @@
-// import { LOGIN_LOADING, LOGIN_SUCCESS, LOGIN_FAILURE } from '../actions/LoginAction';
+import { LOGIN_LOADING, LOGIN_SUCCESS, LOGIN_FAILURE } from '../actions/LoginAction';
 
-// const initialState = {
-//     user: "",
-//     error: null,
-//     loggedIn: false
-// }
+const initialState = {
+    user: "",
+    error: null,
+    loggedIn: false
+}
 
-// export const reducer = (state = initialState, action) => {
-//     console.log("reducer", action);
-//     switch (action.type) {
-//         case LOGIN_LOADING:
-//             return {
-//                 ...state,
-//                 error: null,
-//                 loggedIn: true
-//             }
-//         case LOGIN_SUCCESS:
-//             return {
-//                 ...state,
-//                 credentials: action.payload,
-//                 loggedIn: false
-//             }
-//         case LOGIN_FAILURE:
-//             return {
-//                 ...state,
-//                 credentials:"",
-//                 error: action.payload,
-//                 loggedIn: false
-//             }
-//         default:
-//             return state;
-//     }
-// }
+export const reducer = (state = initialState, action) => {
+    console.log("reducer", action);
+    switch (action.type) {
+        case LOGIN_LOADING:
+            return {
+                ...state,
+                error: null,
+                loggedIn: true
+            }
+        case LOGIN_SUCCESS:
+            return {
+                ...state,
+                credentials: action.payload,
+                loggedIn: false
+            }
+        case LOGIN_FAILURE:
+            return {
+                ...state,
+                credentials:"",
+                error: action.payload,
+                loggedIn: false
+            }
+        default:
+            return state;
+    }
+}
